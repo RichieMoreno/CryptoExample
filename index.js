eefe51f96c26fae7c07c65760aa91a56194f5d9d
Richie Moreno <ricky.92.moreno@gmail.com>
2020-09-01 17:01:40 -0600

    se agrega RSA

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 const crypto = require('crypto');
+const NodeRSA = require('node-rsa');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcom to AES and RSA, encryption examples');
 });
 
 app.post('/encrypt', (req, res) => {
   const encrypted = encryptAES(req.body.text);
-    // res.send("HO");
   res.send({data: encrypted});
 });
 
@@ -20,15 +20,25 @@ app.post('/decrypt', (req, res) => {
     res.send({data: decrypt});
 });
 
+app.post('/encrypt-rsa', (req, res) => {
+    const key = new NodeRSA({b: 256});
+    key.generateKeyPair();
+    const privateDer = key.exportKey('private');
+    console.log("privateDer ", privateDer);
+  const encrypted = key.encrypt(req.body.text, 'base64');
+  res.send({data: encrypted, key: privateDer});
+});
+
+app.post('/decrypt-rsa', (req, res) => {
+    const key = new NodeRSA(req.body.key,'private');
+    const decrypt = key.decrypt(req.body.encrypted, 'utf8');
+    res.send({data: decrypt});
+});
+
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!')
 });
 
-/**
- * Cifra una cadena de texto con el algoritmo aes256 a partir de una llave
- * @param {String} key llave de cifrado
- * @param {String} value valor a cifrar
- */
 function encryptAES(value) {
     const key = crypto.randomBytes(32);
     const iv = crypto.randomBytes(16);
@@ -38,34 +48,9 @@ function encryptAES(value) {
     return {key: key.toString('hex'), iv: iv.toString('hex'), encrypted: encrypted.toString('hex')};
 }
 
-/**
- * Descifra una cadena de texto con el algoritmo aes256 a partir de una llave
- * @param {String} key llave de descifrado
- * @param {String} value valor a descifrar
- */
 function decryptAES(key, iv, encrypted){
     const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(key, 'hex'), Buffer.from(iv, 'hex'));
     let decrypted = decipher.update(Buffer.from(encrypted, 'hex'));
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return decrypted.toString();
-}
-
-/**
- * Cifra una cadena de texto de forma asímétrica utilizando la llave pública obtenída en el método #publicRSAKey
- * @param {String} value valor a cifrar
- */
-function encryptRSA(value) {
-    const buffer = Buffer.from(value);
-    const encrypted = crypto.publicEncrypt(publicRSAKey(), buffer);
-    return encrypted.toString("base64");
-}
-
-/**
- * Descifra una cadena de texto de forma asímétrica utilizando la llave privada obtenída en el método #privateRSAKey
- * @param {*} value
- */
-function decryptRSA(value) {
-    const buffer = Buffer.from(value, "base64");
-    const decrypted = crypto.privateDecrypt(privateRSAKey(), buffer);
-    return decrypted.toString("utf8");
 }
